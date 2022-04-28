6f9d4da284a5d38edc6a732f5099d27e892fc964
liamshatzel <liam.shatzel@gmail.com>
2022-04-27 19:03:17 -0700

    perspective projection

--- src/main.c
+++ src/main.c
@@ -15,7 +15,9 @@ const int N_POINTS = 9 * 9 * 9;
 vec3_t cube_points[N_POINTS]; //creating a cube of size 9x9x9
 vec2_t projected_points[N_POINTS];
 
-float fov_factor = 128;
+vec3_t camera_position = {.x =0, .y = 0, .z = -5 };
+
+float fov_factor = 640;
 
 bool is_running = false;
 
@@ -68,8 +70,8 @@ void process_input(void){
 
 vec2_t project(vec3_t point){
 	vec2_t projected_point = {
-		.x = (fov_factor * point.x),
-		.y = (fov_factor * point.y)
+		.x = (fov_factor * point.x) / point.z,
+		.y = (fov_factor * point.y) / point.z
 	};
 	return projected_point;
 }
@@ -78,6 +80,9 @@ void update(void){
 	for(int i = 0; i < N_POINTS; i++){
 		vec3_t point = cube_points[i];
 		
+		//Move the points away from the camera			
+		point.z -= camera_position.z;		
+
 		//Project the current point
 		vec2_t projected_point = project(point);
 		
